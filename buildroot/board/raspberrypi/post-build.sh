2fedf61a489d6c5023131512f91e46baaa48bae5
gube <gube@localhost.localdomain>
2016-12-19 07:53:35 -1000

    Update: Building rpi3_defconfig should work. The problem had to do with serial communication problems. Verify that your /boot/config.txt contains enable_uart=1 to be sure that it will work.

--- buildroot/board/raspberrypi/post-build.sh
+++ buildroot/board/raspberrypi/post-build.sh
@@ -7,5 +7,5 @@ set -e
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
     grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
 	sed -i '/GENERIC_SERIAL/a\
-tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+tty1::respawn:/sbin/getty -L tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
--- buildroot/board/raspberrypi/post-image.sh
+++ buildroot/board/raspberrypi/post-image.sh
@@ -2,7 +2,7 @@
 
 BOARD_DIR="$(dirname $0)"
 BOARD_NAME="$(basename ${BOARD_DIR})"
-GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
+GENIMAGE_CFG="${BOARD_DIR}/genimage-raspberrypi3.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 # Mark the kernel as DT-enabled
